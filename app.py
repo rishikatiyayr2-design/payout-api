89ab3f2c6b4fbb4270e7a1f44c607c3b077165ac
rishikatiyayr2-design <rishikatiyayr2@gmail.com>
2026-05-16 11:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,39 +5,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    try:
+        url = "https://apilist.tronscanapi.com/api/new/token_trc20/transfers?limit=1&start=0&sort=-timestamp"
 
-    url = "https://api.trongrid.io/v1/transactions"
+        response = requests.get(url).json()
 
-    r = requests.get(url)
-    data = r.json()
+        tx = response["token_transfers"][0]
 
-    txs = data["data"]
+        amount = float(tx["quant"]) / 1000000
 
-    for tx in txs:
+        # Min-Max control
+        if amount < 5:
+            amount = 5
 
-        try:
-            contract = tx["raw_data"]["contract"][0]
+        if amount > 50:
+            amount = 50
 
-            if contract["type"] == "TransferContract":
+        wallet = tx["to_address"]
+        hash_value = tx["transaction_id"]
 
-                value = contract["parameter"]["value"]
+        return {
+            "amount": str(amount),
+            "wallet": wallet,
+            "hash": hash_value
+        }
 
-                amount = value["amount"] / 1000000
-
-                wallet = value["to_address"]
-
-                txhash = tx["txID"]
-
-                return {
-                    "amount": amount,
-                    "hash": txhash,
-                    "wallet": wallet
-                }
-
-        except:
-            pass
-
-    return {"error": "No TRX transaction found"}
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
