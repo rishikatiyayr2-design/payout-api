e5d7310975f5219226947d29aaf30d99183c98a0
rishikatiyayr2-design <rishikatiyayr2@gmail.com>
2026-05-16 11:26:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,45 @@
 from flask import Flask
 import requests
-import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     try:
-        # Latest TRX transaction
         url = "https://apilist.tronscanapi.com/api/transaction"
 
         params = {
             "sort": "-timestamp",
             "count": "true",
-            "limit": 1,
+            "limit": 20,
             "start": 0
         }
 
-        response = requests.get(url, params=params)
+        response = requests.get(url, params=params).json()
 
-        data = response.json()
+        txs = response["data"]
 
-        tx = data["data"][0]
+        trx_tx = None
 
-        hash_value = tx["hash"]
+        for tx in txs:
 
-        wallet = tx["toAddress"]
+            # Sirf TRX transfer
+            if tx.get("contractType") == 1:
 
-        amount = round(random.uniform(5, 50), 2)
+                trx_tx = tx
+                break
+
+        if not trx_tx:
+            return {"error": "No TRX transaction found"}
+
+        hash_value = trx_tx["hash"]
+
+        wallet = trx_tx["toAddress"]
+
+        amount = round(
+            float(trx_tx["amount"]) / 1000000,
+            2
+        )
 
         return {
             "amount": str(amount),
@@ -42,5 +54,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-
-
